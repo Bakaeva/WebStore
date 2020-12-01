a19ec8be5f30eddfd8d10f135796dc8f2e058735
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-12-01 11:36:46 +0300

    Добавьте файлы проекта.

---
+++ WebStore/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        readonly IConfiguration _Configuration;
+
+        public Startup(IConfiguration Configuration)
+        {
+            _Configuration = Configuration;
+        }
+
+        // to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
