3583a0990d87b045ca1dc8c5bf0e5e6e76527a07
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-12-18 12:53:10 +0300

    Создание сервиса для работы с продуктами (пока он только возвращает списки категорий и брендов)

---
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    public interface IProductData
+    {
+        ​ IEnumerable<Section> GetSections();
+        ​ IEnumerable<Brand> GetBrands();
+    }
+}
---
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Data;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Section> GetSections() => TestData.Sections;
+        
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;
+    }
+}
