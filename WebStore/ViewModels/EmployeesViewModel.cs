12e259a53b6bab81210871d5863afcf6f08ddbe3
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-12-18 09:38:56 +0300

    Валидация серверная

--- WebStore/ViewModels/EmployeesViewModel.cs
+++ WebStore/ViewModels/EmployeesViewModel.cs
@@ -1,27 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.ViewModels
 {
-    public class EmployeesViewModel
+    public class EmployeesViewModel //: IValidatableObject
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
 
         /// <summary>Имя</summary>
+        [Display(Name = "Имя")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Имя должно быть длиной от 2 до 20 символов")]
+        [RegularExpression(@"([А-ЯЁ][а-яё]+)|([A-Z][a-z]+)", ErrorMessage = "Ошибка формата имени")]
+        [Required(ErrorMessage = "Поле 'Имя' должно быть заполнено!")]
         public string FirstName { get; set; }
 
         /// <summary>Фамилия</summary>
+        [Display(Name = "Фамилия")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Фамилия должна быть длиной от 2 до 20 символов")]
+        [Required(ErrorMessage = "Поле 'Фамилия' должно быть заполнено!")]
         public string LastName { get; set; }
 
         /// <summary>Отчество</summary>
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
 
         /// <summary>Возраст</summary>
+        [Display(Name = "Возраст")]
+        [Range(18, 80, ErrorMessage = "Возраст сотрудника должен быть от 18 до 80 лет")]
         public int Age { get; set; }
 
         /// <summary>Дата устройства на работу</summary>
+        [Display(Name = "Дата устройства на работу")]
         public DateTime DateOfEmployment { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext context)
+        //{
+        //    yield return ValidationResult.Success;
+        //}
     }
 }
