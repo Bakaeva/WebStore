19cf86a514df614f665768862560668b750cb3e4
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2020-12-24 10:38:20 +0300

    Реализована логика регистрации пользователя

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        readonly UserManager<User> _userManager;
+        readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        #region Регистрация нового пользователя
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = new User
+            {
+                UserName = model.UserName
+            };
+
+            var registration_result = await _userManager.CreateAsync(user, model.Password);
+            if (registration_result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, Role.User);
+
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(model);
+        }
+        #endregion
+    }
+}
