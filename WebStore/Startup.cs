7cd4ad2990eae95ba5d624c2d361c720a3d4d101
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2021-02-09 17:18:20 +0300

    Область Admin, заготовка

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -121,6 +121,10 @@ namespace WebStore
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
