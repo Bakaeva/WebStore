f5518f5f401b1fca068d93e17fcaccaf58d4da6f
Lyudmila Bakaeva <l_bakaeva74@mail.ru>
2021-02-10 17:02:03 +0300

    Вход/выход из области администратора + защита доступа

--- WebStore/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore/Areas/Admin/Controllers/CatalogController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class CatalogController : Controller
     {
+        private readonly IProductData _productData;
+
+        public CatalogController(IProductData productData) => _productData = productData;
+
         public IActionResult Index() => View();
     }
 }
--- WebStore/Areas/Admin/Controllers/HomeController.cs
+++ WebStore/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebStore.Domain.Entities.Identity;
 
 namespace WebStore.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
         public IActionResult Index()
